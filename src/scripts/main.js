acf58dfc6c407b276d7dfbbb6bc078624c8008c5
GuilhermePimenta21 <guipimentta@gmail.com>
2022-10-24 13:07:22 -0300

    .

---
+++ src/scripts/main.js
@@ -0,0 +1,50 @@
+import {getUser} from './services/user.js'
+import {getRepos} from './services/repository.js'
+import {getEvents} from './services/events.js'
+import {user} from './objects/user.js'
+import {screen} from './objects/screen.js'
+
+document.getElementById('btn-search').addEventListener('click', ()=> {
+    const userName = document.getElementById('input-search').value 
+     if (validateEmptyInput(userName)) return
+         getUserData(userName)
+})
+
+// Event button "Enter"
+document.getElementById('input-search').addEventListener('keyup', (e)=> {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed){
+        if (validateEmptyInput(userName)) return
+        getUserData(userName)
+    }
+})
+
+function validateEmptyInput(userName){
+    if(userName.length === 0 ){
+        alert('Preencha o campo com o nome do usuário do GitHub')
+        return true
+    }
+}
+
+async function getUserData(userName) {
+   const userReponse = await getUser(userName)
+   const repositoriesResponse = await getRepos(userName)
+   const eventsResponse = await getEvents(userName)
+
+    if(userReponse.message === 'Not Found'){
+        alert('Este usuário não existe')
+        return
+    }
+
+   user.setInfo(userReponse)
+   console.log(repositoriesResponse)
+   user.setRepositories(repositoriesResponse)
+   user.setEvents(eventsResponse)
+   screen.renderUser(user)
+}
+
+
+
---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,52 @@
+const screen = {
+    userProfile: document.querySelector(".profile-data"),
+    renderUser(user) {
+      this.userProfile.innerHTML = `<div class= "info">
+                                      <img src="${user.avatarUrl}" alt="Foto do perfil" />
+                                      <div class="data">
+                                          <h1>${user.name ?? "Não possui nome cadastrado 😭"}</h1>
+                                          <p>${user.bio ?? "Não possui bio cadastrada 😭"}</p>
+                                          <div class="networking">
+                                              <strong> Seguindo: <span class="follow">${ user.following}</span></strong>
+                                              <strong> Seguidores: <span class="follow">${user.followers}</span></strong>
+                                          </div>    
+                                      </div>
+                                  </div>`;
+  
+      let repositoriesItensAndInteractions = "";
+      user.repositories.forEach((repo) => repositoriesItensAndInteractions += `<li><a href= "${repo.html_url}" target="_blank">${repo.name}
+                                                                                      <p> <i class="fa-solid fa-star">🍴${repo.forks}</i>
+                                                                                           <i class="fa-solid fa-star">📚${repo.language}</i>
+                                                                                           <i class="fa-solid fa-star">⭐${repo.stargazers_count}</i>
+                                                                                           <i class="fa-solid fa-star">👀${repo.watchers}</i>
+                                                                                      </p>
+                                                                                    </a>
+                                                                                </li>`
+      );
+  
+      if (user.repositories.length > 0)
+        this.userProfile.innerHTML += `<div class="repositories section">
+                                          <h2>Repositórios</h2>
+                                          <ul>${repositoriesItensAndInteractions}</ul>
+                                       </div>`;
+  
+      let eventsRecents = "";
+      user.events.forEach((event) => {
+        if (event.type == "PushEvent" || event.type == "CreateEvent") {
+          if (event.payload.commits) {
+            eventsRecents += `<li><strong>${event.repo.name}</strong>
+                                  <span>${event.payload.commits[0].message}</span>
+                              </li>`;
+          }
+        }
+      });
+  
+      if (user.events.length > 0)
+        this.userProfile.innerHTML += `<div class="events">
+                                          <h2> Eventos </h2>
+                                          <ul>${eventsRecents}</ul>  
+                                       </div>`;
+    },
+  };
+  
+  export { screen };
